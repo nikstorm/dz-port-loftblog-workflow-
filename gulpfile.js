1d5ad850c0c2f253b075de18ab544f5aeb688e73
nikstorm <nikstorm@mail.ru>
2018-08-27 05:57:06 +0300

    Template ready

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require("gulp"),
+	connect = require("gulp-connect"),
+	opn = require("opn");
+
+// Запускаем локальный сервер
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true,
+    port: 8888
+  });
+  opn('http://localhost:8888');
+});
+
+// Работа с HTML
+gulp.task('html', function () {
+  gulp.src('./app/*.html')
+    // .pipe(gulp.dest('./app'))
+    .pipe(connect.reload());
+});
+
+// Работа с CSS
+gulp.task('css',function(){
+	gulp.src('./app/css/*.css')
+		.pipe(connect.reload())
+});
+
+// Работа с JS
+gulp.task('js',function(){
+	gulp.src('./app/js/*.js')
+		.pipe(connect.reload())
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./app/*.html'], ['html']);
+  gulp.watch(['./app/css/*.css'], ['css']);
+  gulp.watch(['./app/js/*.js'], ['js']);
+});
+
+gulp.task('default', ['connect', 'watch']);	
